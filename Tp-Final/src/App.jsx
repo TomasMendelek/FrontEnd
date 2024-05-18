5288b8b0492104948dd1eb4a798ddea6970ad568
tomasmendelek <tomascmendelek@gmail.com>
2024-05-18 15:39:54 -0300

    config MUI5

--- Tp-Final/src/App.jsx
+++ Tp-Final/src/App.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
+import { Button } from "@mui/material"
 
 export const App = () => {
   return (
-    <div>as</div>
+    <>
+    <h1>App</h1>
+    <Button variant='contained'>Mi Primer boton</Button>
+    </>
   )
 }
