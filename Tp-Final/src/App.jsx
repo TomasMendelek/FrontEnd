668aa7f9894eb808046b866db3ed0506eb7de099
tomasmendelek <tomascmendelek@gmail.com>
2024-05-26 13:42:32 -0300

    responsive

--- Tp-Final/src/App.jsx
+++ Tp-Final/src/App.jsx
@@ -1,14 +1,24 @@
-import { Box } from "@mui/material";
+import { Box, useMediaQuery, useTheme } from "@mui/material";
 import { NavDrawer } from "./components/navbar/NavDrawer";
 import { Navbar } from "./components/navbar/Navbar";
 import { useState } from "react";
 import { Midia } from "./components/task/Midia";
 import { Tareas } from "./components/tareas/Tareas";
-import { Mantenimiento } from "./components/mantenimiento/Mantenimiento"
+import { Mantenimiento } from "./components/mantenimiento/Mantenimiento";
 
 export const App = () => {
   const [drawerOpen, setDrawerOpen] = useState(false);
   const [selectedComponent, setSelectedComponent] = useState('Midia');
+
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
+
+  const handleSetSelectedComponent = (component) => {
+    setSelectedComponent(component);
+    if (isMobile) {
+      setDrawerOpen(false);
+    }
+  };
 
   const renderSelectedComponent = () => {
     switch (selectedComponent) {
@@ -29,7 +39,7 @@ export const App = () => {
     <>
       <Navbar drawerOpen={drawerOpen} setDrawerOpen={setDrawerOpen} />
       <Box sx={{ display: "flex", bgcolor: "#eeeeee", height: "100vh" }}>
-        {drawerOpen && <NavDrawer setSelectedComponent={setSelectedComponent} />}
+        {drawerOpen && <NavDrawer setSelectedComponent={handleSetSelectedComponent} />}
         {renderSelectedComponent()}
       </Box>
     </>
